f8720fb18ee581b9018e74720adfce6fd8d7d441
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-05-24 21:13:10 +0200

    We made the desing for desktop

--- src/components/header/DarkMode.js
+++ src/components/header/DarkMode.js
@@ -4,6 +4,10 @@ const DarkMode = styled.span`
   align-self: center;
   font-size: 0.8rem;
   font-weight: 600;
+
+  @media (min-width: 768px) {
+    font-size: 1rem;
+  }
 `;
 
 export default DarkMode;
