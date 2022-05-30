13d26925dc746c01ec69a9cc56d78ab2bbd04508
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-05-30 13:39:28 +0200

    We mange the global state with redux

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { changeMode } from "../store/darkModeSlice";
 import DarkMode from "./DarkMode";
 import StyledHeader from "./StyledHeader";
 import Title from "./Title";
 
 const Header = () => {
+  const isDarkModeOn = useSelector((state) => state.darkMode.darkMode);
+  const dispatch = useDispatch();
+
   return (
     <StyledHeader>
       <Title>Where in the world?</Title>
-      <DarkMode>
+      <DarkMode onClick={() => dispatch(changeMode())}>
         <i className="fa-regular fa-moon"></i>Dark Mode
       </DarkMode>
     </StyledHeader>
